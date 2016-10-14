722e69d60aa7f67e9e70c6a596b61ee62a3135c0
Parth Radia <parthnradia@gmail.com>
2016-10-14 14:13:20 -0700

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # We've disabled omniauth
+  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
