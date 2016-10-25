74b741a303f943fd0fe9b2024eb5839e31e816f3
Chris Laganiere <chrislaganiere@ucla.edu>
2016-10-25 04:11:32 +0000

    add tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 256}
 end
---
+++ test/models/user_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Pink Waters", email: "hardrock@example.com", password: "putapasswordhere")
+  end
+  
+  test "user should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+  
+  test "name maximum length" do
+    @user.name = "a" * 75 + "@example.com"
+    assert_not @user.valid?
+  end
+  
+  test "email maximum length" do
+    @user.email = "a" * 256 + "@example.com"
+    assert_not @user.valid?
+  end
+  
+  test "valid emails should be accepted" do
+    valid_email_addresses = %w[dog@example.com dogood@example.com xxyyxx@pizzaplanet.net]
+    valid_email_addresses.each do |valid_email|
+      @user.email = valid_email
+      assert @user.valid?, "email should be valid: #{valid_email}"
+    end
+  end
+  
+  test "invalid emails should not be accepted" do
+    invalid_email_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_email_addresses.each do |invalid_email|
+      @user.email = invalid_email
+      assert_not @user.valid?, "email should not be valid: #{invalid_email}"
+    end
+  end
+  
+  test "email addresses should be unique" do
+    @dup_user = @user.dup
+    @dup_user.email = @user.email.upcase
+    @user.save
+    assert_not @dup_user.valid?
+  end
+end
